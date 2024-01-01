5dd3b0b9fa9c23cc2b79ef1adef8cb9bdfa39008
Zhenghan Hu <victoriahuhh@gmail.com>
2024-01-01 15:29:23 +0000

    Add a simple multiplier

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Welcome to the calculator!");
+
+Console.Write("Input your first number: ");
+
+int x = int.Parse(Console.ReadLine());
+
+
+Console.Write("Input your second number: ");
+int y = int.Parse(Console.ReadLine());
+
+int z = x + y;
+
+Console.WriteLine("Result: " + z);
 Console.ReadLine();
